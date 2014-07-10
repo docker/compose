0d25e6836e6383cc321c1c0a44dc4e3cedba22eb
Nathan LeClaire <nathan.leclaire@docker.com>
2014-07-09 17:08:53 -0700

    Save work
    Signed-off-by: Nathan LeClaire <nathan.leclaire@docker.com>

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -20,16 +21,73 @@ type Service struct {
 	Ports    []string `yaml:"ports"`
 	Volumes  []string `yaml:"volumes"`
 	Running  bool
+	Logger   ServiceLogger
 }
 
-// TODO: set protocol and address properly
-// (default to "unix" and "/var/run/docker.sock", otherwise use $DOCKER_HOST)
-var cli = dockerClient.NewDockerCli(os.Stdin, os.Stdout, os.Stderr, "tcp", "boot2docker:2375", nil)
+type ServiceLogger struct {
+	ServiceName string
+	Stdout      io.Writer
+	Stderr      io.Writer
+	Stdin       io.ReadCloser
+}
 
-func (s *Service) Run() error {
-	var err error
+type StdWriter struct {
+	ServiceName string
+}
 
-	err = cli.CmdRm("-f", s.Name)
+func (s StdWriter) Write(p []byte) (int, error) {
+	fmt.Println(s.ServiceName)
+	fmt.Println(string(p[:]))
+	return len(p) + len(s.ServiceName), nil
+}
+
+func NewStdWriter(name string) StdWriter {
+	return StdWriter{ServiceName: name}
+}
+
+func NewServiceLogger(name string) ServiceLogger {
+	serviceLogger := ServiceLogger{}
+	serviceLogger.ServiceName = name
+	stdOut := StdWriter{}
+	serviceLogger.Stdout = stdOut
+	return serviceLogger
+}
+
+func isRunning(serviceName string) bool {
+	var (
+		err    error
+		val    []byte
+		reader io.Reader
+		writer io.Writer
+	)
+	cli := dockerClient.NewDockerCli(os.Stdin, writer, os.Stderr, "tcp", "localhost:2375", nil)
+	err = cli.CmdInspect([]string{"-f", "'{{ .State.Running }}'", serviceName}...)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "something went wrong inspecting in isRunning", err)
+		return false
+	} else {
+		_, err = io.Copy(writer, reader)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error Copy from reader in isRunning")
+		}
+		val, err = ioutil.ReadAll(reader)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error ReadAll from reader in isRunning")
+		}
+		return (string(val[:]) == "true")
+	}
+}
+
+func (s Service) Run(errmsg chan error) error {
+	serviceLogger := NewServiceLogger(s.Name)
+	cli := dockerClient.NewDockerCli(os.Stdin, serviceLogger.Stdout, os.Stderr, "tcp", "localhost:2375", nil)
+	/*
+		err := cli.CmdRm("-f", s.Name)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error with removing container")
+			errmsg <- err
+		}
+	*/
 
 	cmd := []string{}
 	if len(s.Links) > 0 {
@@ -37,39 +95,45 @@ func (s *Service) Run() error {
 			cmd = append(cmd, []string{"--link", fmt.Sprintf("%s:%s_1", link, link)}...)
 		}
 	}
-	cmd = append(cmd, []string{"-d", "--name", s.Name, s.Image}...)
+	cmd = append(cmd, []string{"--name", s.Name, s.Image}...)
 	if s.Command != "" {
 		cmd = append(cmd, []string{"sh", "-c", s.Command}...)
 	}
 
-	err = cli.CmdRun(cmd...)
-	if err != nil {
-		return err
-	}
+	fmt.Println(cmd)
+	fmt.Println("running cmd", cmd)
+
+	go func() {
+		err := cli.CmdRun(cmd...)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error in CmdRun of the container")
+			errmsg <- err
+		}
+	}()
 
 	return nil
 }
 
 func runServices(services []Service) error {
-	nRun := len(services)
-	linkResolve := make(map[string]bool)
+	errmsg := make(chan error)
 
 	/* Boot services in proper order */
 	for {
-		for _, service := range services {
-			readyToRun := true
-			for _, link := range service.Links {
-				readyToRun = readyToRun && linkResolve[link]
-			}
-			if readyToRun {
-				err := service.Run()
-				if err != nil {
-					return err
+		select {
+		case err := <-errmsg:
+			return err
+		default:
+			for _, service := range services {
+				readyToRun := true
+				for _, link := range service.Links {
+					readyToRun = readyToRun && isRunning(link)
 				}
-				linkResolve[service.Name] = true
-				nRun--
-				if nRun == 0 {
-					return nil
+				fmt.Println("readyToRun", readyToRun)
+				if readyToRun && !isRunning(service.Name) {
+					go func(service Service) {
+						fmt.Println("running ", service)
+						service.Run(errmsg)
+					}(service)
 				}
 			}
 		}
@@ -79,6 +143,9 @@ func runServices(services []Service) error {
 }
 
 func CmdUp(c *gangstaCli.Context) {
+	// TODO: set protocol and address properly
+	// (default to "unix" and "/var/run/docker.sock", otherwise use $DOCKER_HOST)
+	cli := dockerClient.NewDockerCli(os.Stdin, os.Stdout, os.Stderr, "tcp", "boot2docker:2375", nil)
 	servicesRaw, err := ioutil.ReadFile("fig.yml")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error opening fig.yml file")
