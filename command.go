556d0742a43975a5c3e0a83f77d892ec69abd0e3
Nathan LeClaire <nathan.leclaire@docker.com>
2014-07-23 16:58:45 -0700

    Break timed event code into its own file
    Signed-off-by: Nathan LeClaire <nathan.leclaire@docker.com>

--- command.go
+++ command.go
@@ -21,28 +21,6 @@ import (
 	yaml "gopkg.in/yaml.v1"
 )
 
-type TimedEvent struct {
-	eventName string
-	duration  time.Duration
-}
-
-type timedEventSorter struct {
-	events []TimedEvent
-	by     func(te1, te2 *TimedEvent) bool
-}
-
-func (s *timedEventSorter) Len() int {
-	return len(s.events)
-}
-
-func (s *timedEventSorter) Swap(i, j int) {
-	s.events[i], s.events[j] = s.events[j], s.events[i]
-}
-
-func (s *timedEventSorter) Less(i, j int) bool {
-	return s.by(&s.events[i], &s.events[j])
-}
-
 type RebootStep struct {
 	stepName string
 	method   func() error
