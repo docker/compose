b0f398caaa3b783fc605875614da50a2a2c8ef9b
Daniel Nephin <dnephin@gmail.com>
2014-08-07 12:30:01 -0400

    Resolves #386, tty width on jenkins
    Signed-off-by: Daniel Nephin <dnephin@gmail.com>

--- fig/cli/formatter.py
+++ fig/cli/formatter.py
@@ -4,11 +4,17 @@ import os
 import texttable
 
 
+def get_tty_width():
+    tty_size = os.popen('stty size', 'r').read().split()
+    if len(tty_size) != 2:
+        return 80
+    _, width = tty_size
+    return width
+
+
 class Formatter(object):
     def table(self, headers, rows):
-        height, width = os.popen('stty size', 'r').read().split()
-
-        table = texttable.Texttable(max_width=width)
+        table = texttable.Texttable(max_width=get_tty_width())
         table.set_cols_dtype(['t' for h in headers])
         table.add_rows([headers] + rows)
         table.set_deco(table.HEADER)
