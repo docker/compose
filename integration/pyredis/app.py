672a72ab58d1def18049043cc7a48e7a93ae0660
Nathan LeClaire <nathan.leclaire@docker.com>
2014-08-23 18:59:31 -0700

    Add first integration test
    Signed-off-by: Nathan LeClaire <nathan.leclaire@docker.com>

---
+++ integration/pyredis/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from redis import Redis
+import os
+app = Flask(__name__)
+redis = Redis(host="redis_1", port=6379)
+
+@app.route('/')
+def hello():
+    redis.incr('hits')
+    return 'Hello World! I have been seen %s times.' % redis.get('hits')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
