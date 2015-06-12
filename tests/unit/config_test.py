668c83292f373b09ca389d82066c505f82a97ceb
Seb Glazebrook <sebastian.glazebrook@redbubble.com>
2015-06-12 13:03:01 +1000

    Initial ideas around handling env var interpolation in docker-compose.yml
    Signed-off-by: Seb Glazebrook <sebastian.glazebrook@redbubble.com>

--- tests/unit/config_test.py
+++ tests/unit/config_test.py
@@ -504,3 +504,53 @@ class BuildPathTest(unittest.TestCase):
     def test_from_file(self):
         service_dict = config.load('tests/fixtures/build-path/docker-compose.yml')
         self.assertEquals(service_dict, [{'name': 'foo', 'build': self.abs_context_path}])
+
+
+class ComposeTemplateTest(unittest.TestCase):
+
+    def test_render_does_not_affect_strings_without_env_var_references(self):
+        template_string = "some nice easy string"
+        self.assertEqual(config.ComposeTemplate(template_string).render(), "some nice easy string")
+
+    def test_render_evaluates_env_vars(self):
+        template_string = "${MY_NAME_IS}"
+        os.environ["MY_NAME_IS"] = "Slim Shady"
+        self.assertEqual(config.ComposeTemplate(template_string).render(), "Slim Shady")
+
+    def test_render_returns_empty_string_for_non_existant_env_vars(self):
+        template_string = "${MY_NAME_IS}"
+        os.environ.pop("MY_NAME_IS", None)
+        self.assertEqual(config.ComposeTemplate(template_string).render(), "")
+
+    def test_render_returns_default_value_when_default_set_and_env_var_exists(self):
+        template_string = "${MY_NAME_IS-Eminem}"
+        os.environ["MY_NAME_IS"] = "Slim Shady"
+        self.assertEqual(config.ComposeTemplate(template_string).render(), "Slim Shady")
+
+    def test_render_returns_default_value_when_default_set_and_env_var_non_existant(self):
+        template_string = "${MY_NAME_IS-Eminem}"
+        os.environ.pop("MY_NAME_IS", None)
+        self.assertEqual(config.ComposeTemplate(template_string).render(), "Eminem")
+
+    def test_render_evaluates_when_trailing_question_mark_added_and_env_var_exists(self):
+        template_string = "${MY_NAME_IS?}"
+        os.environ["MY_NAME_IS"] = "Slim Shady"
+        self.assertEqual(config.ComposeTemplate(template_string).render(), "Slim Shady")
+
+    def test_render_throws_when_trailing_question_mark_added_and_non_existent_env_var(self):
+        template_string = "${MY_NAME_IS?}"
+        os.environ.pop("MY_NAME_IS", None)
+        with self.assertRaises(config.ConfigurationError):
+            config.ComposeTemplate(template_string).render()
+
+    def test_render_evalutes_multiple_variable_references(self):
+        template_string = "${GREETING-Hi!} ${STATEMENT?}, what, ${STATEMENT?}, who, ${STATEMENT?}, chi i chi i ${MY_NAME_IS-Eminem}."
+        os.environ["STATEMENT"] = "my name is"
+        os.environ["MY_NAME_IS"] = "Slim Shady"
+        self.assertEqual(config.ComposeTemplate(template_string).render(), "Hi! my name is, what, my name is, who, my name is, chi i chi i Slim Shady.")
+
+    def test_render_evalutes_multi_line_strings(self):
+        template_string = "${GREETING-Hi!}\n${STATEMENT?}\nWhat?\n${STATEMENT?}\nWho?\n${STATEMENT?}\nchi i chi i ${MY_NAME_IS-Eminem}"
+        os.environ["STATEMENT"] = "My name is.."
+        os.environ["MY_NAME_IS"] = "Slim Shady"
+        self.assertEqual(config.ComposeTemplate(template_string).render(), "Hi!\nMy name is..\nWhat?\nMy name is..\nWho?\nMy name is..\nchi i chi i Slim Shady")
