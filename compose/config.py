eebec5c34c608b6eb82b42da9640fc0968e91e80
Seb Glazebrook <sebastian.glazebrook@redbubble.com>
2015-06-12 13:19:29 +1000

    Output line number when raising exception
    Signed-off-by: Seb Glazebrook <sebastian.glazebrook@redbubble.com>

--- compose/config.py
+++ compose/config.py
@@ -480,11 +480,13 @@ class ComposeTemplate(object):
 
     def render(self):
         try:
+            self.lines_parsed = 0
             return "\n".join(reduce(self.__evaluate_env_vars, self.string.splitlines(), []))
         except TemplateError as e:
-            raise ConfigurationError(six.text_type(e))
+          raise ConfigurationError("Line " + str(self.lines_parsed) + ": "+ six.text_type(e))
 
     def __evaluate_env_vars(self, memo, line):
+        self.lines_parsed = self.lines_parsed + 1
         line_result = re.sub('\${[^}]*}', self.__match_obj_to_env_var, line)
         memo.append(line_result)
         return memo
