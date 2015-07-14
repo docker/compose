7937611366f58d5d1d5a87cb973b72ec492b2e6e
Erik Kristensen <erik@erikkristensen.com>
2015-07-14 12:30:54 -0600

    change 80 to 0
    Signed-off-by: Erik Kristensen <erik@erikkristensen.com>

--- compose/cli/formatter.py
+++ compose/cli/formatter.py
@@ -3,9 +3,18 @@ from __future__ import absolute_import
 import os
 import texttable
 
+
+def get_tty_width():
+    tty_size = os.popen('stty size', 'r').read().split()
+    if len(tty_size) != 2:
+        return 0
+    _, width = tty_size
+    return int(width)
+
+
 class Formatter(object):
     def table(self, headers, rows):
-        table = texttable.Texttable(max_width=0)
+        table = texttable.Texttable(max_width=get_tty_width())
         table.set_cols_dtype(['t' for h in headers])
         table.add_rows([headers] + rows)
         table.set_deco(table.HEADER)
