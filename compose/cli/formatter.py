b7edb3ca9d21faa2497d1d15bf3ce2c6c56a1029
Erik Kristensen <erik@erikkristensen.com>
2015-07-14 12:03:11 -0600

    do not wrap table cells
    Signed-off-by: Erik Kristensen <erik@erikkristensen.com>

--- compose/cli/formatter.py
+++ compose/cli/formatter.py
@@ -3,18 +3,9 @@ from __future__ import absolute_import
 import os
 import texttable
 
-
-def get_tty_width():
-    tty_size = os.popen('stty size', 'r').read().split()
-    if len(tty_size) != 2:
-        return 80
-    _, width = tty_size
-    return int(width)
-
-
 class Formatter(object):
     def table(self, headers, rows):
-        table = texttable.Texttable(max_width=get_tty_width())
+        table = texttable.Texttable(max_width=0)
         table.set_cols_dtype(['t' for h in headers])
         table.add_rows([headers] + rows)
         table.set_deco(table.HEADER)
