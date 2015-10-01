757d7a14a24c4efdcfb7bdb937ce2245f1b0f30e
Marco Enrico Piras <kikkomep@crs4.it>
2015-10-01 06:09:15 +0200

    Fix container name replacing '_' with '-'
    Signed-off-by: Marco Enrico Piras <kikkomep@crs4.it>

--- compose/service.py
+++ compose/service.py
@@ -853,7 +853,7 @@ def build_container_name(project, service, number, one_off=False):
     bits = [project, service]
     if one_off:
         bits.append('run')
-    return '_'.join(bits + [str(number)])
+    return '-'.join(bits + [str(number)])
 
 
 # Images
