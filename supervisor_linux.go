8f8f365787de515b20c1cc091bcd76be31acc155
Michael Crosby <crosbymichael@gmail.com>
2015-12-02 10:59:43 -0800

    Add linux import path
    Signed-off-by: Michael Crosby <crosbymichael@gmail.com>

--- supervisor_linux.go
+++ supervisor_linux.go
@@ -2,7 +2,10 @@
 
 package containerd
 
-import "github.com/docker/containerd/runtime"
+import (
+	"github.com/docker/containerd/linux"
+	"github.com/docker/containerd/runtime"
+)
 
 func newRuntime(stateDir string) (runtime.Runtime, error) {
 	return linux.NewRuntime(stateDir)
