6bfc4df8a7f97792aa0b90402543f5e5084660a7
Alexander Morozov <lk4d4@docker.com>
2015-12-08 10:16:30 -0800

    Log start tasks
    Signed-off-by: Alexander Morozov <lk4d4@docker.com>

--- worker.go
+++ worker.go
@@ -4,6 +4,7 @@ import (
 	"sync"
 	"time"
 
+	"github.com/Sirupsen/logrus"
 	"github.com/docker/containerd/runtime"
 )
 
@@ -33,6 +34,11 @@ func (w *worker) Start() {
 	defer w.wg.Done()
 	for t := range w.s.tasks {
 		started := time.Now()
+		logrus.WithFields(logrus.Fields{
+			"containerID": t.Container.ID(),
+			"checkpoint":  t.Checkpoint,
+			"started":     started,
+		}).Debug("worker received task")
 		if t.Checkpoint != "" {
 			if err := t.Container.Restore(t.Checkpoint); err != nil {
 				evt := NewEvent(DeleteEventType)
