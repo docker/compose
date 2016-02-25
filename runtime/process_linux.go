1acf68535aff908bcf737f9248af2e17b758235a
John Howard <jhoward@microsoft.com>
2016-02-25 12:59:34 -0800

    Refactor process.go for platform specific
    Signed-off-by: John Howard <jhoward@microsoft.com>

---
+++ runtime/process_linux.go
@@ -0,0 +1,27 @@
+package runtime
+
+import (
+	"os"
+	"syscall"
+)
+
+func getExitPipe(path string) (*os.File, error) {
+	if err := syscall.Mkfifo(path, 0755); err != nil && !os.IsExist(err) {
+		return nil, err
+	}
+	// add NONBLOCK in case the other side has already closed or else
+	// this function would never return
+	return os.OpenFile(path, syscall.O_RDONLY|syscall.O_NONBLOCK, 0)
+}
+
+func getControlPipe(path string) (*os.File, error) {
+	if err := syscall.Mkfifo(path, 0755); err != nil && !os.IsExist(err) {
+		return nil, err
+	}
+	return os.OpenFile(path, syscall.O_RDWR|syscall.O_NONBLOCK, 0)
+}
+
+// Signal sends the provided signal to the process
+func (p *process) Signal(s os.Signal) error {
+	return syscall.Kill(p.pid, s.(syscall.Signal))
+}
