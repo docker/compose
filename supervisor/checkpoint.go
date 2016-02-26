cdb5947f862cfba606bf48d934bde432d1d84d59
John Howard <jhoward@microsoft.com>
2016-02-25 19:18:10 -0800

    Supervisor compiles
    Signed-off-by: John Howard <jhoward@microsoft.com>

--- supervisor/checkpoint.go
+++ supervisor/checkpoint.go
@@ -1,3 +1,5 @@
+// +build !windows
+
 package supervisor
 
 import "github.com/docker/containerd/runtime"
--- supervisor/supervisor.go
+++ supervisor/supervisor.go
@@ -216,42 +216,6 @@ func (s *Supervisor) Start() error {
 	return nil
 }
 
-func (s *Supervisor) handleTask(i Task) {
-	var err error
-	switch t := i.(type) {
-	case *AddProcessTask:
-		err = s.addProcess(t)
-	case *CreateCheckpointTask:
-		err = s.createCheckpoint(t)
-	case *DeleteCheckpointTask:
-		err = s.deleteCheckpoint(t)
-	case *StartTask:
-		err = s.start(t)
-	case *DeleteTask:
-		err = s.delete(t)
-	case *ExitTask:
-		err = s.exit(t)
-	case *ExecExitTask:
-		err = s.execExit(t)
-	case *GetContainersTask:
-		err = s.getContainers(t)
-	case *SignalTask:
-		err = s.signal(t)
-	case *StatsTask:
-		err = s.stats(t)
-	case *UpdateTask:
-		err = s.updateContainer(t)
-	case *UpdateProcessTask:
-		err = s.updateProcess(t)
-	default:
-		err = ErrUnknownTask
-	}
-	if err != errDeferedResponse {
-		i.ErrorCh() <- err
-		close(i.ErrorCh())
-	}
-}
-
 // Machine returns the machine information for which the
 // supervisor is executing on.
 func (s *Supervisor) Machine() Machine {
