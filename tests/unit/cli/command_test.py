fd020ed2cfa2fb2b03a31a3fe87d6da47edfd713
Joffrey F <joffrey@docker.com>
2016-03-07 17:46:55 -0800

    Tests use updated get_config_paths_from_options signature
    Signed-off-by: Joffrey F <joffrey@docker.com>

--- tests/unit/cli/command_test.py
+++ tests/unit/cli/command_test.py
@@ -15,24 +15,24 @@ class TestGetConfigPathFromOptions(object):
     def test_path_from_options(self):
         paths = ['one.yml', 'two.yml']
         opts = {'--file': paths}
-        assert get_config_path_from_options(opts) == paths
+        assert get_config_path_from_options('.', opts) == paths
 
     def test_single_path_from_env(self):
         with mock.patch.dict(os.environ):
             os.environ['COMPOSE_FILE'] = 'one.yml'
-            assert get_config_path_from_options({}) == ['one.yml']
+            assert get_config_path_from_options('.', {}) == ['one.yml']
 
     @pytest.mark.skipif(IS_WINDOWS_PLATFORM, reason='posix separator')
     def test_multiple_path_from_env(self):
         with mock.patch.dict(os.environ):
             os.environ['COMPOSE_FILE'] = 'one.yml:two.yml'
-            assert get_config_path_from_options({}) == ['one.yml', 'two.yml']
+            assert get_config_path_from_options('.', {}) == ['one.yml', 'two.yml']
 
     @pytest.mark.skipif(not IS_WINDOWS_PLATFORM, reason='windows separator')
     def test_multiple_path_from_env_windows(self):
         with mock.patch.dict(os.environ):
             os.environ['COMPOSE_FILE'] = 'one.yml;two.yml'
-            assert get_config_path_from_options({}) == ['one.yml', 'two.yml']
+            assert get_config_path_from_options('.', {}) == ['one.yml', 'two.yml']
 
     def test_no_path(self):
-        assert not get_config_path_from_options({})
+        assert not get_config_path_from_options('.', {})
