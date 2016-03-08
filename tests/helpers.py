5831b869e879b1350f0610f41b2dbd58c5e6285f
Joffrey F <joffrey@docker.com>
2016-03-07 17:18:19 -0800

    Update tests for new environment handling
    Signed-off-by: Joffrey F <joffrey@docker.com>

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,14 +1,9 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-import functools
-import os
-
-from . import mock
 from compose.config.config import ConfigDetails
 from compose.config.config import ConfigFile
 from compose.config.config import load
-from compose.config.environment import Environment
 
 
 def build_config(contents, **kwargs):
@@ -19,11 +14,3 @@ def build_config_details(contents, working_dir='working_dir', filename='filename
     return ConfigDetails(
         working_dir,
         [ConfigFile(filename, contents)])
-
-
-def clear_environment(f):
-    @functools.wraps(f)
-    def wrapper(self, *args, **kwargs):
-        Environment.reset()
-        with mock.patch.dict(os.environ):
-            f(self, *args, **kwargs)
