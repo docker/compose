165815fdbffd4b771be71fbe207ad906d042ecea
HuKeping <hukeping@huawei.com>
2016-04-22 11:01:24 -0400

    Introduce a ctr wide constant
    Signed-off-by: Hu Keping <hukeping@huawei.com>

---
+++ ctr/const.go
@@ -0,0 +1,10 @@
+package main
+
+// ctr wide constants
+const (
+	// ExitStatusOK indicates successful completion
+	ExitStatusOK = 0
+
+	// ExitStatusMissingArg indicates failure due to missing argument(s)
+	ExitStatusMissingArg = 1
+)
