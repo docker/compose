2580fce8f7999076ec197c216fb9b126e5a7da68
stefan <think@hotmail.de>
2016-06-30 14:07:57 +0200

    added failing test that ensures that named volume will not be printed with a mode suffix
    Signed-off-by: stefan <think@hotmail.de>

--- tests/acceptance/cli_test.py
+++ tests/acceptance/cli_test.py
@@ -192,7 +192,7 @@ class CLITestCase(DockerClientTestCase):
                 'other': {
                     'image': 'busybox:latest',
                     'command': 'top',
-                    'volumes': ['/data:rw'],
+                    'volumes': ['/data'],
                 },
             },
         }
