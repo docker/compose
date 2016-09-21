53fa44c01e7b50b571c48101d6ca59ac0fd94ace
Joffrey F <joffrey@docker.com>
2016-09-20 18:05:59 -0700

    Don't break when interpolating environment with unicode characters
    Signed-off-by: Joffrey F <joffrey@docker.com>

--- compose/service.py
+++ compose/service.py
@@ -1109,6 +1109,8 @@ def format_environment(environment):
     def format_env(key, value):
         if value is None:
             return key
+        if isinstance(value, six.binary_type):
+            value = value.decode('utf-8')
         return '{key}={value}'.format(key=key, value=value)
     return [format_env(*item) for item in environment.items()]
 
