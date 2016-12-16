f867156a38ad74854fb02efb670d3d6fb2d7db28
Phil Estes <estesp@linux.vnet.ibm.com>
2016-12-16 15:20:53 -0500

    Update protoc generation to use vendored protos
    Use vendored-in protos in the project path rather
    than expecting developers to have them in a local
    path on the host. This made a generated change
    in the FileDescriptor content, but everything else
    matches and binaries are building properly.
    
    Signed-off-by: Phil Estes <estesp@linux.vnet.ibm.com>

--- api/execution/gen.go
+++ api/execution/gen.go
@@ -1,3 +1,3 @@
 package execution
 
-//go:generate protoc -I.:../..:../../../../../github.com/gogo/protobuf:/usr/local/include --gogoctrd_out=plugins=grpc,import_path=github.com/docker/containerd/api/execution,Mgogoproto/gogo.proto=github.com/gogo/protobuf/gogoproto,Mgoogle/protobuf/descriptor.proto=github.com/gogo/protobuf/protoc-gen-gogo/descriptor:. execution.proto
+//go:generate protoc -I.:../../vendor:../../vendor/github.com/gogo/protobuf:../../../../../..:/usr/local/include --gogoctrd_out=plugins=grpc,import_path=github.com/docker/containerd/api/execution,Mgogoproto/gogo.proto=github.com/gogo/protobuf/gogoproto,Mgoogle/protobuf/descriptor.proto=github.com/gogo/protobuf/protoc-gen-gogo/descriptor:. execution.proto
