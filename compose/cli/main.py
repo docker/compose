5a18022eecd2387ea488115b9be20774cf78152c
kbroadwater <kbroadwater@isilon.com>
2017-02-10 10:35:08 -0800

    Returing 1 when a container exits with a non-zero exit code with --abort-on-container-exit is set. Signed-off-by: Kevin Broadwater <kevin.broadwater@gmail.com>

--- compose/cli/main.py
+++ compose/cli/main.py
@@ -914,7 +914,13 @@ class TopLevelCommand(object):
 
             if cascade_stop:
                 print("Aborting on container exit...")
+                exit_code = 0
+                for e in self.project.containers(service_names=options['SERVICE'], stopped=True):
+                    if not e.is_running:
+                        if not e.exit_code == 0:
+                            exit_code = 1
                 self.project.stop(service_names=service_names, timeout=timeout)
+                sys.exit(exit_code)
 
     @classmethod
     def version(cls, options):
