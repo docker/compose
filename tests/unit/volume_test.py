75fe64b7b92bda63b17eeeb80eab29bb9610ef27
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2017-03-04 21:37:36 +0100

    \#3731 Allows volumes to specify an `actual_name` property, allowing to by pass some volume driver/hosting limitations
    Signed-off-by: Damien Coraboeuf <damien.coraboeuf@gmail.com>

--- tests/unit/volume_test.py
+++ tests/unit/volume_test.py
@@ -24,3 +24,15 @@ class TestVolume(object):
         vol = volume.Volume(mock_client, 'foo', 'project', external_name='data')
         vol.remove()
         assert not mock_client.remove_volume.called
+
+    def test_full_name_volume(self, mock_client):
+        vol = volume.Volume(mock_client, 'project', 'foo')
+        assert vol.full_name == 'project_foo'
+
+    def test_full_name_volume_with_actual_name(self, mock_client):
+        vol = volume.Volume(mock_client, 'project', 'foo', actual_name='my-foo')
+        assert vol.full_name == 'my-foo'
+
+    def test_full_name_volume_with_external_name(self, mock_client):
+        vol = volume.Volume(mock_client, 'project', 'foo', external_name='my-external-foo')
+        assert vol.full_name == 'my-external-foo'
