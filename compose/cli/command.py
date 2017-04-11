0f00aa409861f4f7820a21055d25ec1f312db7d0
Joffrey F <joffrey@docker.com>
2017-04-10 17:32:10 -0700

    Convert paths to unicode in get_config_path_from_options if needed
    Signed-off-by: Joffrey F <joffrey@docker.com>

--- compose/cli/command.py
+++ compose/cli/command.py
@@ -49,14 +49,17 @@ def get_config_from_options(base_dir, options):
 
 
 def get_config_path_from_options(base_dir, options, environment):
+    def unicode_paths(paths):
+        return [p.decode('utf-8') if isinstance(p, six.binary_type) else p for p in paths]
+
     file_option = options.get('--file')
     if file_option:
-        return file_option
+        return unicode_paths(file_option)
 
     config_files = environment.get('COMPOSE_FILE')
     if config_files:
         pathsep = environment.get('COMPOSE_PATH_SEPARATOR', os.pathsep)
-        return config_files.split(pathsep)
+        return unicode_paths(config_files.split(pathsep))
     return None
 
 
