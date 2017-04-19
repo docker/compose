e9ee69e813e71657779bdd8aa2b846afa20315e5
Joffrey F <joffrey@docker.com>
2017-04-19 16:47:43 -0700

    Prevent `docker-compose scale` to be used with a v2.2 config file
    Signed-off-by: Joffrey F <joffrey@docker.com>

--- compose/service.py
+++ compose/service.py
@@ -384,8 +384,8 @@ class Service(object):
                 lambda n: self.get_container_name(n),
                 "Creating"
             )
-            if errors:
-                raise OperationFailedError(errors.values()[0])
+            for error in errors.values():
+                raise OperationFailedError(error)
 
             return containers
 
@@ -405,8 +405,9 @@ class Service(object):
                 lambda c: c.name,
                 "Recreating"
             )
-            if errors:
-                raise OperationFailedError(errors.values()[0])
+            for error in errors.values():
+                raise OperationFailedError(error)
+
             if len(containers) < scale:
                 containers.extend(self._execute_convergence_create(
                     scale - len(containers), detached, start
@@ -425,8 +426,8 @@ class Service(object):
                     "Starting"
                 )
 
-                if errors:
-                    raise OperationFailedError(errors.values()[0])
+                for error in errors.values():
+                    raise OperationFailedError(error)
 
             if len(containers) < scale:
                 containers.extend(self._execute_convergence_create(
