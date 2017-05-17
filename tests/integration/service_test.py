bffdb7a349f4db65535dd4fce0b946c30c4d3030
Alexey Rokhin <arokhin@mail.ru>
2017-05-17 16:42:43 +0300

    service_test.py reorder imports
    Signed-off-by: Alexey Rokhin <arokhin@mail.ru>

--- tests/integration/service_test.py
+++ tests/integration/service_test.py
@@ -19,13 +19,13 @@ from .testcases import pull_busybox
 from compose import __version__
 from compose.config.types import VolumeFromSpec
 from compose.config.types import VolumeSpec
+from compose.const import IS_WINDOWS_PLATFORM
 from compose.const import LABEL_CONFIG_HASH
 from compose.const import LABEL_CONTAINER_NUMBER
 from compose.const import LABEL_ONE_OFF
 from compose.const import LABEL_PROJECT
 from compose.const import LABEL_SERVICE
 from compose.const import LABEL_VERSION
-from compose.const import IS_WINDOWS_PLATFORM
 from compose.container import Container
 from compose.errors import OperationFailedError
 from compose.project import OneOffFilter
