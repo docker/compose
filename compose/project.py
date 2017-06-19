docker/compose

commit e9b6cc23fcf01d4768c7e082b7bc91b43ff84e7e

Add --quiet parameter to docker-compose pull, using existing silent flag
Signed-off-by: Joel Barciauskas <barciajo@gmail.com>

compose/project.py
@@ -462,12 +462,12 @@ class Project(object):
 
         return plans
 
-    def pull(self, service_names=None, ignore_pull_failures=False, parallel_pull=False):
+    def pull(self, service_names=None, ignore_pull_failures=False, parallel_pull=False, silent=False):
         services = self.get_services(service_names, include_deps=False)
 
         if parallel_pull:
             def pull_service(service):
-                service.pull(ignore_pull_failures, True)
+                service.pull(ignore_pull_failures, True, silent=silent)
 
             parallel.parallel_execute(
                 services,
@@ -477,7 +477,7 @@ class Project(object):
                 limit=5)
         else:
             for service in services:
-                service.pull(ignore_pull_failures)
+                service.pull(ignore_pull_failures, silent=silent)
 
     def push(self, service_names=None, ignore_push_failures=False):
         for service in self.get_services(service_names, include_deps=False):

compose/cli/main.py
@@ -634,11 +634,13 @@ class TopLevelCommand(object):
         Options:
             --ignore-pull-failures  Pull what it can and ignores images with pull failures.
             --parallel              Pull multiple images in parallel.
+            --quiet                 Pull without printing progress information
         """
         self.project.pull(
             service_names=options['SERVICE'],
             ignore_pull_failures=options.get('--ignore-pull-failures'),
-            parallel_pull=options.get('--parallel')
+            parallel_pull=options.get('--parallel'),
+            silent=options.get('--quiet'),
         )
 
     def push(self, options):

tests/acceptance/cli_test.py
@@ -431,6 +431,10 @@ class CLITestCase(DockerClientTestCase):
         assert ('repository nonexisting-image not found' in result.stderr or
                 'image library/nonexisting-image:latest not found' in result.stderr)
 
+    def test_pull_with_quiet(self):
+        assert self.dispatch(['pull', '--quiet']).stderr == ''
+        assert self.dispatch(['pull', '--quiet']).stdout == ''
+
     def test_build_plain(self):
         self.base_dir = 'tests/fixtures/simple-dockerfile'
         self.dispatch(['build', 'simple'])

commit 811afbc15c980e9832b9ecdf0d54d196741bb4a5

Fix double silent argument value
Fix for "TypeError: pull() got multiple values for keyword argument 'silent'."
This change https://github.com/docker/compose/commit/e9b6cc23fcf01d4768c7e082b7bc91b43ff84e7e caused additional value to be passed for the 'silent' argument, that was already passed there: https://github.com/docker/compose/commit/f85da99ef3273794e855afda8678174419d3bf4f

Signed-off-by: Nikita Vlaznev <nikita.dto@gmail.com>

compose/project.py
@@ -467,7 +467,7 @@ class Project(object):
 
         if parallel_pull:
             def pull_service(service):
-                service.pull(ignore_pull_failures, True, silent=silent)
+                service.pull(ignore_pull_failures, silent=silent)
 
             parallel.parallel_execute(
                 services,