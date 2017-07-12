344a69331cb3842c6f2f9f18eef22dea10ca21a5
Joffrey F <joffrey@docker.com>
2017-07-11 19:07:12 -0700

    Bump 1.15.0-rc1
    Signed-off-by: Joffrey F <joffrey@docker.com>

--- tests/integration/testcases.py
+++ tests/integration/testcases.py
@@ -17,6 +17,7 @@ from compose.const import COMPOSEFILE_V2_0 as V2_0
 from compose.const import COMPOSEFILE_V2_0 as V2_1
 from compose.const import COMPOSEFILE_V2_2 as V2_2
 from compose.const import COMPOSEFILE_V3_0 as V3_0
+from compose.const import COMPOSEFILE_V3_2 as V3_2
 from compose.const import COMPOSEFILE_V3_3 as V3_3
 from compose.const import LABEL_PROJECT
 from compose.progress_stream import stream_output
@@ -52,7 +53,7 @@ def engine_max_version():
     if version_lt(version, '1.13'):
         return V2_1
     if version_lt(version, '17.06'):
-        return V2_2
+        return V3_2
     return V3_3
 
 
@@ -72,7 +73,7 @@ def v2_1_only():
 
 
 def v2_2_only():
-    return min_version_skip(V2_0)
+    return min_version_skip(V2_2)
 
 
 def v3_only():
