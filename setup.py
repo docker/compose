bde87e462357541acefeeb97a9b3c46ded6fbb29
Joffrey F <joffrey@docker.com>
2017-08-02 17:02:30 -0700

    Bump texttable dependency
    Signed-off-by: Joffrey F <joffrey@docker.com>

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ install_requires = [
     'docopt >= 0.6.1, < 0.7',
     'PyYAML >= 3.10, < 4',
     'requests >= 2.6.1, != 2.11.0, < 2.12',
-    'texttable >= 0.8.1, < 0.9',
+    'texttable >= 0.9.0, < 0.10',
     'websocket-client >= 0.32.0, < 1.0',
     'docker >= 2.4.2, < 3.0',
     'dockerpty >= 0.4.1, < 0.5',
