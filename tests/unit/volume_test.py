122367681c5e0f9badaa24d9518ca8a95665f354
Joffrey F <joffrey@docker.com>
2017-08-09 19:43:08 -0700

    Add support for v3.4 files and custom volume names
    Signed-off-by: Joffrey F <joffrey@docker.com>

--- tests/unit/volume_test.py
+++ tests/unit/volume_test.py
@@ -21,6 +21,6 @@ class TestVolume(object):
         mock_client.remove_volume.assert_called_once_with('foo_project')
 
     def test_remove_external_volume(self, mock_client):
-        vol = volume.Volume(mock_client, 'foo', 'project', external_name='data')
+        vol = volume.Volume(mock_client, 'foo', 'project', external=True)
         vol.remove()
         assert not mock_client.remove_volume.called
