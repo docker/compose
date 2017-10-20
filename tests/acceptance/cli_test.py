a1a6fb485b40cc2f4fff19fc7f5067fcf0292bfa
Guillermo Arribas <garribas@gmail.com>
2017-10-19 22:07:30 -0300

    docker-compose exec doesn't have -e option (fixes #4551)
    Signed-off-by: Guillermo Arribas <garribas@gmail.com>

--- tests/acceptance/cli_test.py
+++ tests/acceptance/cli_test.py
@@ -33,6 +33,7 @@ from tests.integration.testcases import no_cluster
 from tests.integration.testcases import pull_busybox
 from tests.integration.testcases import SWARM_SKIP_RM_VOLUMES
 from tests.integration.testcases import v2_1_only
+from tests.integration.testcases import v2_2_only
 from tests.integration.testcases import v2_only
 from tests.integration.testcases import v3_only
 
@@ -1392,6 +1393,31 @@ class CLITestCase(DockerClientTestCase):
         stdout, stderr = self.dispatch(['exec', '-T', '--user=operator', 'console', 'whoami'])
         self.assertEqual(stdout, "operator\n")
         self.assertEqual(stderr, "")
+
+    @v2_2_only()
+    def test_exec_service_with_environment_overridden(self):
+        name = 'service'
+        self.base_dir = 'tests/fixtures/environment-exec'
+        self.dispatch(['up', '-d'])
+        self.assertEqual(len(self.project.containers()), 1)
+
+        stdout, stderr = self.dispatch([
+            'exec',
+            '-T',
+            '-e', 'foo=notbar',
+            '--env', 'alpha=beta',
+            name,
+            'env',
+        ])
+
+        # env overridden
+        assert 'foo=notbar' in stdout
+        # keep environment from yaml
+        assert 'hello=world' in stdout
+        # added option from command line
+        assert 'alpha=beta' in stdout
+
+        self.assertEqual(stderr, '')
 
     def test_run_service_without_links(self):
         self.base_dir = 'tests/fixtures/links-composefile'
