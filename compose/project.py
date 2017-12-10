d334e92df9eeaa755b75788c29c10b4daa12c42c
Ian Glen Neal <ian.gl.neal@gmail.com>
2017-12-10 23:50:24 +0000

    Fix #5465 by catching a no-image exception in get_container_data_volumes. The ImageNotFound exception is now bubbled up to the client, which prompts the user on the desired course of action (rebuild or abort). Added a case to the unit test to check that an empty existing image doesn't result in an exception.
    Closed old pull request #5466 because I did a rebase and it showed over
    300 commits to merge, which I thought was messy.
    
    Signed-off-by: Ian Glen Neal <ian.gl.neal@gmail.com>

--- compose/project.py
+++ compose/project.py
@@ -441,6 +441,7 @@ class Project(object):
            remove_orphans=False,
            ignore_orphans=False,
            scale_override=None,
+           rebuild=False,
            rescale=True,
            start=True,
            always_recreate_deps=False):
@@ -471,6 +472,7 @@ class Project(object):
                 timeout=timeout,
                 detached=detached,
                 scale_override=scale_override.get(service.name),
+                rebuild=rebuild,
                 rescale=rescale,
                 start=start,
                 project_services=scaled_services
