eba67910f3b59f121e0832e4edb01c64b05a10b9
Joffrey F <joffrey@docker.com>
2018-04-20 16:21:13 -0700

    Containerize release tool
    Signed-off-by: Joffrey F <joffrey@docker.com>

---
+++ script/release/release.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+docker image inspect compose/release-tool > /dev/null
+if test $? -ne 0; then
+    docker build -t compose/release-tool -f $(pwd)/script/release/Dockerfile $(pwd)
+fi
+
+if test -z $GITHUB_TOKEN; then
+    echo "GITHUB_TOKEN environment variable must be set"
+    exit 1
+fi
+
+if test -z $BINTRAY_TOKEN; then
+    echo "BINTRAY_TOKEN environment variable must be set"
+    exit 1
+fi
+
+docker run -e GITHUB_TOKEN=$GITHUB_TOKEN -e BINTRAY_TOKEN=$BINTRAY_TOKEN -it \
+    --mount type=bind,source=$(pwd),target=/src \
+    --mount type=bind,source=$(pwd)/.git,target=/src/.git \
+    --mount type=bind,source=$HOME/.docker,target=/root/.docker \
+    --mount type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock \
+    --mount type=bind,source=$HOME/.ssh,target=/root/.ssh \
+    -v $HOME/.pypirc:/root/.pypirc \
+    compose/release-tool $*
