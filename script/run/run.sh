d04333bd32a30b9485b56368a04585bbda517bc8
Joakim Roubert <joakimr@axis.com>
2019-03-11 08:06:55 +0100

    run.sh: Performance improvements
    Some shell script best practices applied; remove unnecessary extra if
    statements, remove quoting where not needed, use shell internal
    functionality instead of external call to dirname.
    
    Change-Id: I71b64a82e7e907a85822bd147d7c116c87ffea09
    Signed-off-by: Joakim Roubert <joakimr@axis.com>

--- script/run/run.sh
+++ script/run/run.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -e
 #
 # Run docker-compose in a container
 #
@@ -12,52 +12,37 @@
 # the $COMPOSE_OPTIONS environment variable.
 #
 
-
-set -e
-
-VERSION="1.23.2"
+VERSION=1.23.2
 IMAGE="docker/compose:$VERSION"
 
 
 # Setup options for connecting to docker host
-if [ -z "$DOCKER_HOST" ]; then
-    DOCKER_HOST="/var/run/docker.sock"
-fi
+[ -n "$DOCKER_HOST" ] || DOCKER_HOST=/var/run/docker.sock
 if [ -S "$DOCKER_HOST" ]; then
-    DOCKER_ADDR="-v $DOCKER_HOST:$DOCKER_HOST -e DOCKER_HOST"
+	DOCKER_ADDR="-v $DOCKER_HOST:$DOCKER_HOST -e DOCKER_HOST"
 else
-    DOCKER_ADDR="-e DOCKER_HOST -e DOCKER_TLS_VERIFY -e DOCKER_CERT_PATH"
+	DOCKER_ADDR="-e DOCKER_HOST -e DOCKER_TLS_VERIFY -e DOCKER_CERT_PATH"
 fi
 
 
 # Setup volume mounts for compose config and context
-if [ "$(pwd)" != '/' ]; then
-    VOLUMES="-v $(pwd):$(pwd)"
-fi
-if [ -n "$COMPOSE_FILE" ]; then
-    COMPOSE_OPTIONS="$COMPOSE_OPTIONS -e COMPOSE_FILE=$COMPOSE_FILE"
-    compose_dir=$(realpath $(dirname $COMPOSE_FILE))
-fi
+[ "$(pwd)" == '/' ] || VOLUMES="-v $(pwd):$(pwd)"
+[ -z "$COMPOSE_FILE" ] || {
+	COMPOSE_OPTIONS="$COMPOSE_OPTIONS -e COMPOSE_FILE=$COMPOSE_FILE"
+	compose_dir=$(realpath ${COMPOSE_FILE%/*})
+}
 # TODO: also check --file argument
-if [ -n "$compose_dir" ]; then
-    VOLUMES="$VOLUMES -v $compose_dir:$compose_dir"
-fi
-if [ -n "$HOME" ]; then
-    VOLUMES="$VOLUMES -v $HOME:$HOME -v $HOME:/root" # mount $HOME in /root to share docker.config
-fi
+[ -z "$compose_dir" ] || VOLUMES="$VOLUMES -v $compose_dir:$compose_dir"
+[ -z "$HOME" ] || VOLUMES="$VOLUMES -v $HOME:$HOME -v $HOME:/root" # mount $HOME in /root to share docker.config
 
 # Only allocate tty if we detect one
-if [ -t 0 -a -t 1 ]; then
-        DOCKER_RUN_OPTIONS="$DOCKER_RUN_OPTIONS -t"
-fi
+[ -t 0 ] && [ -t 1 ] && DOCKER_RUN_OPTIONS="$DOCKER_RUN_OPTIONS -t"
 
 # Always set -i to support piped and terminal input in run/exec
 DOCKER_RUN_OPTIONS="$DOCKER_RUN_OPTIONS -i"
 
 
 # Handle userns security
-if [ ! -z "$(docker info 2>/dev/null | grep userns)" ]; then
-    DOCKER_RUN_OPTIONS="$DOCKER_RUN_OPTIONS --userns=host"
-fi
+[ -z "$(docker info 2>/dev/null | grep userns)" ] || DOCKER_RUN_OPTIONS="$DOCKER_RUN_OPTIONS --userns=host"
 
-exec docker run --rm $DOCKER_RUN_OPTIONS $DOCKER_ADDR $COMPOSE_OPTIONS $VOLUMES -w "$(pwd)" $IMAGE "$@"
+exec docker run --rm $DOCKER_RUN_OPTIONS $DOCKER_ADDR $COMPOSE_OPTIONS $VOLUMES -w "$(pwd)" $IMAGE $*
