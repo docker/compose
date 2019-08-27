60458c8ae7c3d99a4dd408bc7fbced3b4a8cd7de
Lumir Balhar <lbalhar@redhat.com>
2019-08-27 12:25:20 +0200

    Implement custom context manager for changing CWD
    Signed-off-by: Lumír Balhar <lbalhar@redhat.com>

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
+import contextlib
 import os
 
 from compose.config.config import ConfigDetails
@@ -55,3 +56,17 @@ def create_host_file(client, filename):
         content = fh.read()
 
     return create_custom_host_file(client, filename, content)
+
+
+@contextlib.contextmanager
+def cd(path):
+    """
+    A context manager which changes the working directory to the given
+    path, and then changes it back to its previous value on exit.
+    """
+    prev_cwd = os.getcwd()
+    os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(prev_cwd)
