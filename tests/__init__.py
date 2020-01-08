c44746d92d696d64386145216b605733d2e12a37
Bastian Venthur <bastian.venthur@flixbus.com>
2020-01-08 09:52:04 +0100

    Removed now useless check for version_info >= 2.7
    Signed-off-by: Bastian Venthur <bastian.venthur@flixbus.com>

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,4 @@
-import sys
-
-if sys.version_info >= (2, 7):
-    import unittest  # NOQA
-else:
-    import unittest2 as unittest  # NOQA
+import unittest  # NOQA
 
 try:
     from unittest import mock
