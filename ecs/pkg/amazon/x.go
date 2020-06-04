7337c7520fad4f6ecf752b60567aca9870c3ca9c
aiordache <anca.iordache@docker.com>
2020-06-04 18:23:31 +0200

    rename LB field in the compose file
    Signed-off-by: aiordache <anca.iordache@docker.com>
    Signed-off-by: Nicolas De Loof <nicolas.deloof@gmail.com>

--- ecs/pkg/amazon/x.go
+++ ecs/pkg/amazon/x.go
@@ -4,5 +4,5 @@ const (
 	ExtensionSecurityGroup   = "x-aws-securitygroup"
 	ExtensionVPC             = "x-aws-vpc"
 	ExtensionPullCredentials = "x-aws-pull_credentials"
-	ExtensionLB              = "x-aws-lb"
+	ExtensionLB              = "x-aws-loadbalancer"
 )
