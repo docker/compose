e56061d27c3dcf4e5cc662cf52e5a276cc6a287b
Guillaume Tardif <guillaume.tardif@docker.com>
2020-09-15 17:28:23 +0200

    Move fire and forget code from metrics.Track() (used only by CLI) to metrics.Send (used by both CLI and API)
    Signed-off-by: Guillaume Tardif <guillaume.tardif@docker.com>

--- metrics/client.go
+++ metrics/client.go
@@ -64,10 +64,24 @@ func NewClient() Client {
 }
 
 func (c *client) Send(command Command) {
-	req, err := json.Marshal(command)
-	if err != nil {
-		return
-	}
+	wasIn := make(chan bool)
 
-	_, _ = c.httpClient.Post("http://localhost/usage", "application/json", bytes.NewBuffer(req))
+	// Fire and forget, we don't want to slow down the user waiting for DD
+	// metrics endpoint to respond. We could lose some events but that's ok.
+	go func() {
+		defer func() {
+			_ = recover()
+		}()
+
+		wasIn <- true
+
+		req, err := json.Marshal(command)
+		if err != nil {
+			return
+		}
+
+		_, _ = c.httpClient.Post("http://localhost/usage", "application/json", bytes.NewBuffer(req))
+	}()
+	<-wasIn
+
 }
