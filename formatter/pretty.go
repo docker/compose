8c51b8b67dbcbe9e3f1d76b745d564e11c7c780a
Ulysses Souza <ulyssessouza@gmail.com>
2020-10-01 05:21:06 +0200

    Fix lint
    Signed-off-by: Ulysses Souza <ulyssessouza@gmail.com>

--- formatter/pretty.go
+++ formatter/pretty.go
@@ -26,7 +26,7 @@ import (
 // PrintPrettySection prints a tabbed section on the writer parameter
 func PrintPrettySection(out io.Writer, printer func(writer io.Writer), headers ...string) error {
 	w := tabwriter.NewWriter(out, 20, 1, 3, ' ', 0)
-	fmt.Fprintln(w, strings.Join(headers, "\t"))
+	_, _ = fmt.Fprintln(w, strings.Join(headers, "\t"))
 	printer(w)
 	return w.Flush()
 }
