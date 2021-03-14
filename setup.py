6e0317929a1f9e0858503a6b007af04b98ef7d2f
Nicholas Sturgeon <sturgeon.nick@icloud.com>
2021-03-14 11:25:37 -0400

    Update dependencies to support TOML configurations
    Support for TOML configuration files alongside YAML requires that
    the 'toml' package be included in the main set of requirements.
    
    - Move the 'toml' package from the indirect requirements file to
    the main requirements file.
    - Add the 'toml' package to the dev requirements file.
    - Add the 'toml' package to the setup script.
    
    Signed-off-by: Nicholas Sturgeon <sturgeon.nick@icloud.com>

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ install_requires = [
     'cached-property >= 1.2.0, < 2',
     'docopt >= 0.6.1, < 1',
     'PyYAML >= 3.10, < 6',
+    'toml >= 0.10.2, < 1',
     'requests >= 2.20.0, < 3',
     'texttable >= 0.9.0, < 2',
     'websocket-client >= 0.32.0, < 1',
