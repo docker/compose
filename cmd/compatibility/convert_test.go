2540f37268ec45db99d91d3f77a76f5eade29b88
Nicolas De Loof <nicolas.deloof@gmail.com>
2021-12-08 17:59:48 +0100

    handle "stop" event
    Signed-off-by: Nicolas De Loof <nicolas.deloof@gmail.com>

--- cmd/compatibility/convert_test.go
+++ cmd/compatibility/convert_test.go
@@ -14,7 +14,7 @@
    limitations under the License.
 */
 
-package main
+package compatibility
 
 import (
 	"testing"
@@ -71,7 +71,7 @@ func Test_convert(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := convert(tt.args)
+			got := Convert(tt.args)
 			assert.DeepEqual(t, tt.want, got)
 		})
 	}
