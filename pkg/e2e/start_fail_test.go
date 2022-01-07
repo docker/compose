3eeed88cd2696a24c8abdccaae402473f7108166
Nikhil Benesch <nikhil.benesch@gmail.com>
2022-01-07 12:40:59 -0500

    Don't wait forever for unhealthy dependencies
    The previous code would wait for dependencies to become healthy forever,
    even if they'd become unhealthy in the meantime. I can't find an issue
    report for this bug, but it was described in a comment on the PR that
    introduced the `--wait` flag [0].
    
    [0]: https://github.com/docker/compose/pull/8777#issuecomment-965643839
    
    Signed-off-by: Nikhil Benesch <nikhil.benesch@gmail.com>

---
+++ pkg/e2e/start_fail_test.go
@@ -0,0 +1,33 @@
+/*
+   Copyright 2020 Docker Compose CLI authors
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package e2e
+
+import (
+	"testing"
+
+	"gotest.tools/v3/icmd"
+)
+
+func TestStartFail(t *testing.T) {
+	c := NewParallelE2eCLI(t, binDir)
+	const projectName = "e2e-start-fail"
+
+	res := c.RunDockerOrExitError("compose", "-f", "fixtures/start-fail/compose.yaml", "--project-name", projectName, "up", "-d")
+	res.Assert(t, icmd.Expected{ExitCode: 1, Err: `container for service "fail" is unhealthy`})
+
+	c.RunDockerComposeCmd("--project-name", projectName, "down")
+}
