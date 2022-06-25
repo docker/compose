2cb1d1c90f398396b8ea337d2647f8a5b79f7f30
seunggabi <seunggabi@naver.com>
2022-06-25 16:30:15 +0900

    (#8097) fix: [yaml 1.2.2] boolean (true, false)
    Signed-off-by: seunggabi <seunggabi@naver.com>

--- compose/config/serialize.py
+++ compose/config/serialize.py
@@ -21,10 +21,10 @@ def serialize_string(dumper, data):
     if isinstance(data, bytes):
         data = data.decode('utf-8')
 
-    if data.lower() in ('y', 'n', 'yes', 'no', 'on', 'off', 'true', 'false'):
+    if data.lower() in ('true', 'false'):
         # Empirically only y/n appears to be an issue, but this might change
         # depending on which PyYaml version is being used. Err on safe side.
-        return dumper.represent_scalar('tag:yaml.org,2002:str', data, style='"')
+        return dumper.represent_scalar('tag:yaml.org,2002:bool', data, style='"')
     return representer(data)
 
 
