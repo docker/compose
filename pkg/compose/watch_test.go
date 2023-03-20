a11515e038e909be55e44e9da0ba2923a0ec2758
Nicolas De Loof <nicolas.deloof@gmail.com>
2023-03-20 10:25:23 +0100

    introduce `ignore` attribute for watch triggers
    Signed-off-by: Nicolas De Loof <nicolas.deloof@gmail.com>

--- pkg/compose/watch_test.go
+++ pkg/compose/watch_test.go
@@ -17,7 +17,10 @@ package compose
 import (
 	"context"
 	"testing"
+	"time"
 
+	"github.com/docker/cli/cli/command"
+	"github.com/docker/compose/v2/pkg/watch"
 	"github.com/jonboulle/clockwork"
 	"golang.org/x/sync/errgroup"
 	"gotest.tools/v3/assert"
@@ -44,7 +47,7 @@ func Test_debounce(t *testing.T) {
 		return nil
 	})
 	for i := 0; i < 100; i++ {
-		ch <- fileMapping{service: "test"}
+		ch <- fileMapping{Service: "test"}
 	}
 	assert.Equal(t, ran, 0)
 	clock.Advance(quietPeriod)
@@ -52,4 +55,96 @@ func Test_debounce(t *testing.T) {
 	assert.NilError(t, err)
 	assert.Equal(t, ran, 1)
 	assert.DeepEqual(t, got, []string{"test"})
+}
+
+type testWatcher struct {
+	events chan watch.FileEvent
+	errors chan error
+}
+
+func (t testWatcher) Start() error {
+	return nil
+}
+
+func (t testWatcher) Close() error {
+	return nil
+}
+
+func (t testWatcher) Events() chan watch.FileEvent {
+	return t.events
+}
+
+func (t testWatcher) Errors() chan error {
+	return t.errors
+}
+
+func Test_sync(t *testing.T) {
+	needSync := make(chan fileMapping)
+	needRebuild := make(chan fileMapping)
+	ctx, cancelFunc := context.WithCancel(context.TODO())
+	defer cancelFunc()
+
+	run := func() watch.Notify {
+		watcher := testWatcher{
+			events: make(chan watch.FileEvent, 1),
+			errors: make(chan error),
+		}
+
+		go func() {
+			cli, err := command.NewDockerCli()
+			assert.NilError(t, err)
+
+			service := composeService{
+				dockerCli: cli,
+			}
+			err = service.watch(ctx, "test", watcher, []Trigger{
+				{
+					Path:   "/src",
+					Action: "sync",
+					Target: "/work",
+					Ignore: []string{"ignore"},
+				},
+				{
+					Path:   "/",
+					Action: "rebuild",
+				},
+			}, needSync, needRebuild)
+			assert.NilError(t, err)
+		}()
+		return watcher
+	}
+
+	t.Run("synchronize file", func(t *testing.T) {
+		watcher := run()
+		watcher.Events() <- watch.NewFileEvent("/src/changed")
+		select {
+		case actual := <-needSync:
+			assert.DeepEqual(t, fileMapping{Service: "test", HostPath: "/src/changed", ContainerPath: "/work/changed"}, actual)
+		case <-time.After(100 * time.Millisecond):
+			t.Error("timeout")
+		}
+	})
+
+	t.Run("ignore", func(t *testing.T) {
+		watcher := run()
+		watcher.Events() <- watch.NewFileEvent("/src/ignore")
+		select {
+		case <-needSync:
+			t.Error("file event should have been ignored")
+		case <-time.After(100 * time.Millisecond):
+			// expected
+		}
+	})
+
+	t.Run("rebuild", func(t *testing.T) {
+		watcher := run()
+		watcher.Events() <- watch.NewFileEvent("/dependencies.yaml")
+		select {
+		case event := <-needRebuild:
+			assert.Equal(t, "test", event.Service)
+		case <-time.After(100 * time.Millisecond):
+			t.Error("timeout")
+		}
+	})
+
 }
