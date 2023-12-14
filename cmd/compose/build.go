e4fb5545f02e859b4de8031d927204973d84e67c
Nicolas De Loof <nicolas.deloof@gmail.com>
2023-12-14 11:08:15 +0100

    build do not require environment to be reslved
    Signed-off-by: Nicolas De Loof <nicolas.deloof@gmail.com>

--- cmd/compose/build.go
+++ cmd/compose/build.go
@@ -131,7 +131,7 @@ func buildCommand(p *ProjectOptions, dockerCli command.Cli, backend api.Service)
 }
 
 func runBuild(ctx context.Context, dockerCli command.Cli, backend api.Service, opts buildOptions, services []string) error {
-	project, err := opts.ToProject(dockerCli, services, cli.WithResolvedPaths(true))
+	project, err := opts.ToProject(dockerCli, services, cli.WithResolvedPaths(true), cli.WithoutEnvironmentResolution)
 	if err != nil {
 		return err
 	}
