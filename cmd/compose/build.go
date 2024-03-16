22459435f432a2aba12974e2012a74319ec9a5bf
Felix Fontein <felix@fontein.de>
2024-03-16 17:10:58 +0100

    Use rawjson for the build backend.
    Signed-off-by: Felix Fontein <felix@fontein.de>

--- cmd/compose/build.go
+++ cmd/compose/build.go
@@ -67,10 +67,9 @@ func (opts buildOptions) toAPIBuildOptions(services []string) (api.BuildOptions,
 		builderName = os.Getenv("BUILDX_BUILDER")
 	}
 
-	// The build backends do not have a "json" progress; use "plain" instead
 	uiMode := ui.Mode
 	if uiMode == ui.ModeJSON {
-		uiMode = ui.ModePlain
+		uiMode = "rawjson"
 	}
 	return api.BuildOptions{
 		Pull:     opts.pull,
