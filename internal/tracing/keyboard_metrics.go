2ed40e804209dfd6e49628ecd4e262d4d6346b49
Guillaume Lours <705411+glours@users.noreply.github.com>
2024-05-23 18:36:57 +0200

    add gui/composeview as part of available commands
    Signed-off-by: Guillaume Lours <705411+glours@users.noreply.github.com>

--- internal/tracing/keyboard_metrics.go
+++ internal/tracing/keyboard_metrics.go
@@ -22,7 +22,7 @@ import (
 	"go.opentelemetry.io/otel/attribute"
 )
 
-func KeyboardMetrics(ctx context.Context, enabled, isDockerDesktopActive, isWatchConfigured bool) {
+func KeyboardMetrics(ctx context.Context, enabled, isDockerDesktopActive, isWatchConfigured, isDockerDesktopComposeUI bool) {
 	commandAvailable := []string{}
 	if isDockerDesktopActive {
 		commandAvailable = append(commandAvailable, "gui")
@@ -30,6 +30,11 @@ func KeyboardMetrics(ctx context.Context, enabled, isDockerDesktopActive, isWatc
 	if isWatchConfigured {
 		commandAvailable = append(commandAvailable, "watch")
 	}
+
+	if isDockerDesktopComposeUI {
+		commandAvailable = append(commandAvailable, "gui/composeview")
+	}
+
 	AddAttributeToSpan(ctx,
 		attribute.Bool("navmenu.enabled", enabled),
 		attribute.StringSlice("navmenu.command_available", commandAvailable))
