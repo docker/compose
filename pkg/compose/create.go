c13725f2b7920f8f60c1fca9a641bbb3159ef745
Suleiman Dibirov <idsulik@gmail.com>
2024-06-23 19:10:20 +0300

    fix(hash): recreate container on project config content change
    Signed-off-by: Suleiman Dibirov <idsulik@gmail.com>

--- pkg/compose/create.go
+++ pkg/compose/create.go
@@ -212,7 +212,7 @@ func (s *composeService) getCreateConfigs(ctx context.Context,
 	inherit *moby.Container,
 	opts createOptions,
 ) (createConfigs, error) {
-	labels, err := s.prepareLabels(opts.Labels, service, number)
+	labels, err := s.prepareLabels(opts.Labels, p, service, number)
 	if err != nil {
 		return createConfigs{}, err
 	}
@@ -498,8 +498,8 @@ func parseSecurityOpts(p *types.Project, securityOpts []string) ([]string, bool,
 	return parsed, unconfined, nil
 }
 
-func (s *composeService) prepareLabels(labels types.Labels, service types.ServiceConfig, number int) (map[string]string, error) {
-	hash, err := ServiceHash(service)
+func (s *composeService) prepareLabels(labels types.Labels, project *types.Project, service types.ServiceConfig, number int) (map[string]string, error) {
+	hash, err := ServiceHash(project, service)
 	if err != nil {
 		return nil, err
 	}
