af5b748500fd240d3b7058fbaa5f956766ba9c05
Nicolas De Loof <nicolas.deloof@gmail.com>
2024-11-28 09:35:45 +0100

    revisit TestDebounceBatching
    Signed-off-by: Nicolas De Loof <nicolas.deloof@gmail.com>

--- pkg/compose/watch.go
+++ pkg/compose/watch.go
@@ -344,7 +344,7 @@ func batchDebounceEvents(ctx context.Context, clock clockwork.Clock, delay time.
 	out := make(chan []fileEvent)
 	go func() {
 		defer close(out)
-		seen := make(map[sync.PathMapping]fileEvent)
+		seen := make(map[string]fileEvent)
 		flushEvents := func() {
 			if len(seen) == 0 {
 				return
@@ -354,7 +354,7 @@ func batchDebounceEvents(ctx context.Context, clock clockwork.Clock, delay time.
 				events = append(events, e)
 			}
 			out <- events
-			seen = make(map[sync.PathMapping]fileEvent)
+			seen = make(map[string]fileEvent)
 		}
 
 		t := clock.NewTicker(delay)
@@ -371,7 +371,7 @@ func batchDebounceEvents(ctx context.Context, clock clockwork.Clock, delay time.
 					flushEvents()
 					return
 				}
-				seen[e.PathMapping] = e
+				seen[e.HostPath] = e
 				t.Reset(delay)
 			}
 		}
