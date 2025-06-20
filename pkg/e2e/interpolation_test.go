cc226280f58bab2173966df62bd2bc9adb9d237c
Guillaume Lours <705411+glours@users.noreply.github.com>
2025-06-20 11:07:17 +0200

    add e2e tests to check interpolation in environment files
    Signed-off-by: Guillaume Lours <705411+glours@users.noreply.github.com>

--- pkg/e2e/interpolation_test.go
+++ pkg/e2e/interpolation_test.go
@@ -26,6 +26,7 @@ import (
 	"testing"
 
 	"gotest.tools/v3/assert"
+	"gotest.tools/v3/icmd"
 )
 
 func Test_interpolation(t *testing.T) {
@@ -40,7 +41,7 @@ func Test_interpolation(t *testing.T) {
 	})
 
 	res := c.RunDockerComposeCmd(t, "-f", "fixtures/interpolation/compose.yaml", "--project-name", projectName, "up")
-	env := getEnv(res.Combined())
+	env := getEnv(res.Combined(), false)
 
 	assert.Check(t, slices.Contains(env, "FOO=FOO-from-dot-env"))
 	assert.Check(t, slices.Contains(env, "BAR=bar_from_environment"))
@@ -64,7 +65,7 @@ func Test_interpolationWithInclude(t *testing.T) {
 	})
 
 	res := c.RunDockerComposeCmd(t, "-f", "fixtures/interpolation/include/compose.yaml", "--project-name", projectName, "up")
-	env := getEnv(res.Combined())
+	env := getEnv(res.Combined(), false)
 
 	assert.Check(t, slices.Contains(env, "FOO=FOO-from-include"))
 	assert.Check(t, slices.Contains(env, "BAR=bar_from_environment"))
@@ -87,7 +88,7 @@ func Test_interpolationWithExtends(t *testing.T) {
 	})
 
 	res := c.RunDockerComposeCmd(t, "-f", "fixtures/interpolation/extends/compose.yaml", "--project-name", projectName, "up")
-	env := getEnv(res.Combined())
+	env := getEnv(res.Combined(), false)
 
 	assert.Check(t, slices.Contains(env, "FOO=FOO-from-extends"))
 	assert.Check(t, slices.Contains(env, "BAR=BAR-from-extends"))
@@ -98,13 +99,47 @@ func Test_interpolationWithExtends(t *testing.T) {
 	assert.Check(t, slices.Contains(env, "BY_PROVIDER_FROM_ENV_FILE=https://magic.cloud/example"))
 }
 
-func getEnv(out string) []string {
+func TestInterpolationInEnvFile(t *testing.T) {
+	provider, err := findExecutable("example-provider")
+	assert.NilError(t, err)
+	path := fmt.Sprintf("%s%s%s", os.Getenv("PATH"), string(os.PathListSeparator), filepath.Dir(provider))
+	c := NewParallelCLI(t, WithEnv("PATH="+path))
+
+	const projectName = "interpolation-env-file"
+	t.Log("interpolation in env file from os env and implicit env file")
+	cmd := c.NewDockerComposeCmd(t, "-f", "fixtures/interpolation/compose.yaml", "--project-name", projectName, "up")
+	cmd.Env = append(cmd.Env, "OS_TO_ENV_FILE=OS_TO_ENV_FILE_VALUE")
+	t.Cleanup(func() {
+		c.cleanupWithDown(t, projectName)
+	})
+
+	res := icmd.RunCmd(cmd)
+	assert.NilError(t, res.Error, res.Combined())
+	env := getEnv(res.Combined(), false)
+	assert.Check(t, slices.Contains(env, "BY_OS_FROM_ENV_FILE=OS_TO_ENV_FILE_VALUE"), env)
+	assert.Check(t, slices.Contains(env, "BY_IMPLICIT_ENV_FILE=IMPLICIT_ENV_FILE_VALUE"), env)
+
+	t.Log("interpolation in env file from command env and explicit env file")
+	cmd = c.NewDockerComposeCmd(t, "-f", "fixtures/interpolation/compose.yaml", "--project-name", projectName,
+		"--env-file", "fixtures/interpolation/explicit_env_file.env", "run",
+		"--env", "BY_CMD_FROM_ENV_FILE=CMD_TO_ENV_FILE_VALUE", "--rm", "test")
+
+	res = icmd.RunCmd(cmd)
+	env = getEnv(res.Combined(), true)
+	assert.Check(t, slices.Contains(env, "BY_EXPLICIT_ENV_FILE=EXPLICIT_ENV_FILE_VALUE"), env)
+	assert.Check(t, slices.Contains(env, "BY_CMD_FROM_ENV_FILE=CMD_TO_ENV_FILE_VALUE"), env)
+}
+
+func getEnv(out string, run bool) []string {
 	var env []string
 	scanner := bufio.NewScanner(strings.NewReader(out))
 	for scanner.Scan() {
 		line := scanner.Text()
-		if strings.HasPrefix(line, "test-1  | ") {
+		if !run && strings.HasPrefix(line, "test-1  | ") {
 			env = append(env, line[10:])
+		}
+		if run && strings.Contains(line, "=") && len(strings.Split(line, "=")) == 2 {
+			env = append(env, line)
 		}
 	}
 	slices.Sort(env)
