d19bd5b40557c82672f0aec0abf1d7de0d047390
Nicolas De Loof <nicolas.deloof@gmail.com>
2025-07-17 15:14:58 +0200

    simpler stop UI
    Signed-off-by: Nicolas De Loof <nicolas.deloof@gmail.com>

--- pkg/compose/up.go
+++ pkg/compose/up.go
@@ -92,10 +92,6 @@ func (s *composeService) Up(ctx context.Context, project *types.Project, options
 		}
 	}
 
-	tui := formatter.NewStopping(logConsumer)
-	defer tui.Close()
-	logConsumer = tui
-
 	watcher, err := NewWatcher(project, options, s.watch, logConsumer)
 	if err != nil && options.Start.Watch {
 		return err
@@ -112,7 +108,7 @@ func (s *composeService) Up(ctx context.Context, project *types.Project, options
 		first := true
 		gracefulTeardown := func() {
 			first = false
-			tui.ApplicationTermination()
+			fmt.Println("Gracefully Stopping... press Ctrl+C again to force")
 			eg.Go(func() error {
 				return progress.RunWithLog(context.WithoutCancel(ctx), func(ctx context.Context) error {
 					return s.stop(ctx, project.Name, api.StopOptions{
