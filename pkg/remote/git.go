db2ed2d99c5cd440f35b707c0091d0e6557ab00c
Sebastiaan van Stijn <github@gone.nl>
2025-08-28 21:12:55 +0200

    go.mod: bump buildkit v0.24.0-rc1, buildx v0.28.0-rc1
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- pkg/remote/git.go
+++ pkg/remote/git.go
@@ -32,7 +32,7 @@ import (
 	"github.com/compose-spec/compose-go/v2/types"
 	"github.com/docker/cli/cli/command"
 	"github.com/docker/compose/v2/pkg/api"
-	"github.com/moby/buildkit/util/gitutil"
+	gitutil "github.com/moby/buildkit/frontend/dockerfile/dfgitutil"
 	"github.com/sirupsen/logrus"
 )
 
